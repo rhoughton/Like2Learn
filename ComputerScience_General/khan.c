a3ef22c3a976f854e0769107776813ed9d6e821d
Rob Houghton <houghton.robert.e@gmail.com>
2020-10-24 08:38:18 -0600

    khan study

---
+++ ComputerScience_General/khan.c
@@ -0,0 +1,3 @@
+a is the coefficient of the x squared term, b is the coefficient of the x term and c is the co of the x to the zero or "cnonstant" term 
+quadratic formula is crazy, but worth remembering? Well according to the reddit's math jokes' factoring is the way to go. 
+complete the square 
